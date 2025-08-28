461331d77561935cccb6f5c7b7291f96afd46747
ghaderimasoud568-hue <ghaderimasoud568@gmail.com>
2025-08-28 10:13:17 -0500

    Update main.cpp
    ez fix, learn from me pal 😎

--- BasicGitHubProject/main.cpp
+++ BasicGitHubProject/main.cpp
@@ -1,9 +1,1 @@
-#include<iotream> 
-
-int main()
-{
-	std::cout << "asdfasdfa\n"; //version control
-
-	std:cout << "aasdf1123375859659fa\n"//version control
-	//asfdasdfa
-	//what does \n do? It moves the cursor to a NEWline
+u sux mr.norman 
