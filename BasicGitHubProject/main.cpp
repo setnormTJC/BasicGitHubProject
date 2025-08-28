7f80f692452fad9c601137126358ddec50088e0c
Work <Work@MSI>
2025-08-28 10:02:37 -0500

    I screwed UP the codebase in this, very special commit.

--- BasicGitHubProject/main.cpp
+++ BasicGitHubProject/main.cpp
@@ -1,10 +1,9 @@
-#include<iostream> 
+#include<iotream> 
 
 int main()
 {
 	std::cout << "asdfasdfa\n"; //version control
 
-	std::cout << "aasdf1123375859659fa\n"; //version control
+	std:cout << "aasdf1123375859659fa\n"//version control
 	//asfdasdfa
 	//what does \n do? It moves the cursor to a NEWline
-}
