70b289d5b991206c461c404d118365ef331a29e8
Work <Work@MSI>
2025-08-28 09:34:40 -0500

    I added some FANTASTIC code.

--- BasicGitHubProject/main.cpp
+++ BasicGitHubProject/main.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
-	std::cout << "asdfasdfa";
+	std::cout << "asdfasdfa\n"; //version control
+
+	std::cout << "aasdf1123375859659fa\n"; //version control
+
+	//what does \n do? It moves the cursor to a NEWline
 }
